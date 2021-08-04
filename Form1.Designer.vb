2e8f539ee9c500c80adf1f705c4fad69471ebd83
MontyMango <59061157+MontyMango@users.noreply.github.com>
2021-08-04 14:07:19 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,183 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.LblEnterInt = New System.Windows.Forms.Label()
+        Me.txtIntegerInput = New System.Windows.Forms.TextBox()
+        Me.btnDouble = New System.Windows.Forms.Button()
+        Me.btnHalf = New System.Windows.Forms.Button()
+        Me.btnRandomNum = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnTriple = New System.Windows.Forms.Button()
+        Me.btnQuarter = New System.Windows.Forms.Button()
+        Me.radKeepInput = New System.Windows.Forms.RadioButton()
+        Me.radKeepTotal = New System.Windows.Forms.RadioButton()
+        Me.btnUnselect = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'LblEnterInt
+        '
+        Me.LblEnterInt.AutoSize = True
+        Me.LblEnterInt.Location = New System.Drawing.Point(13, 13)
+        Me.LblEnterInt.Name = "LblEnterInt"
+        Me.LblEnterInt.Size = New System.Drawing.Size(110, 19)
+        Me.LblEnterInt.TabIndex = 0
+        Me.LblEnterInt.Text = "Enter an integer:"
+        '
+        'txtIntegerInput
+        '
+        Me.txtIntegerInput.Location = New System.Drawing.Point(129, 13)
+        Me.txtIntegerInput.Name = "txtIntegerInput"
+        Me.txtIntegerInput.Size = New System.Drawing.Size(62, 26)
+        Me.txtIntegerInput.TabIndex = 1
+        '
+        'btnDouble
+        '
+        Me.btnDouble.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnDouble.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.btnDouble.Location = New System.Drawing.Point(13, 48)
+        Me.btnDouble.Name = "btnDouble"
+        Me.btnDouble.Size = New System.Drawing.Size(86, 26)
+        Me.btnDouble.TabIndex = 2
+        Me.btnDouble.Text = "Double"
+        Me.btnDouble.UseVisualStyleBackColor = True
+        '
+        'btnHalf
+        '
+        Me.btnHalf.ForeColor = System.Drawing.Color.IndianRed
+        Me.btnHalf.Location = New System.Drawing.Point(105, 48)
+        Me.btnHalf.Name = "btnHalf"
+        Me.btnHalf.Size = New System.Drawing.Size(86, 26)
+        Me.btnHalf.TabIndex = 3
+        Me.btnHalf.Text = "Half"
+        Me.btnHalf.UseVisualStyleBackColor = True
+        '
+        'btnRandomNum
+        '
+        Me.btnRandomNum.ForeColor = System.Drawing.Color.Fuchsia
+        Me.btnRandomNum.Location = New System.Drawing.Point(13, 112)
+        Me.btnRandomNum.Name = "btnRandomNum"
+        Me.btnRandomNum.Size = New System.Drawing.Size(178, 26)
+        Me.btnRandomNum.TabIndex = 6
+        Me.btnRandomNum.Text = "Random Number"
+        Me.btnRandomNum.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.DarkSalmon
+        Me.btnExit.ForeColor = System.Drawing.Color.Black
+        Me.btnExit.Location = New System.Drawing.Point(197, 112)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(86, 26)
+        Me.btnExit.TabIndex = 100
+        Me.btnExit.TabStop = False
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btnTriple
+        '
+        Me.btnTriple.ForeColor = System.Drawing.Color.Olive
+        Me.btnTriple.Location = New System.Drawing.Point(13, 80)
+        Me.btnTriple.Name = "btnTriple"
+        Me.btnTriple.Size = New System.Drawing.Size(86, 26)
+        Me.btnTriple.TabIndex = 4
+        Me.btnTriple.Text = "Triple"
+        Me.btnTriple.UseVisualStyleBackColor = True
+        '
+        'btnQuarter
+        '
+        Me.btnQuarter.ForeColor = System.Drawing.Color.Peru
+        Me.btnQuarter.Location = New System.Drawing.Point(105, 80)
+        Me.btnQuarter.Name = "btnQuarter"
+        Me.btnQuarter.Size = New System.Drawing.Size(86, 26)
+        Me.btnQuarter.TabIndex = 5
+        Me.btnQuarter.Text = "Quarter"
+        Me.btnQuarter.UseVisualStyleBackColor = True
+        '
+        'radKeepInput
+        '
+        Me.radKeepInput.AutoSize = True
+        Me.radKeepInput.Location = New System.Drawing.Point(197, 51)
+        Me.radKeepInput.Name = "radKeepInput"
+        Me.radKeepInput.Size = New System.Drawing.Size(94, 23)
+        Me.radKeepInput.TabIndex = 101
+        Me.radKeepInput.Text = "Keep Input"
+        Me.radKeepInput.UseVisualStyleBackColor = True
+        '
+        'radKeepTotal
+        '
+        Me.radKeepTotal.AutoSize = True
+        Me.radKeepTotal.Location = New System.Drawing.Point(198, 81)
+        Me.radKeepTotal.Name = "radKeepTotal"
+        Me.radKeepTotal.Size = New System.Drawing.Size(90, 23)
+        Me.radKeepTotal.TabIndex = 102
+        Me.radKeepTotal.Text = "Keep Total"
+        Me.radKeepTotal.UseVisualStyleBackColor = True
+        '
+        'btnUnselect
+        '
+        Me.btnUnselect.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btnUnselect.Location = New System.Drawing.Point(197, 13)
+        Me.btnUnselect.Name = "btnUnselect"
+        Me.btnUnselect.Size = New System.Drawing.Size(86, 26)
+        Me.btnUnselect.TabIndex = 7
+        Me.btnUnselect.Text = "Unselect"
+        Me.btnUnselect.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(298, 157)
+        Me.Controls.Add(Me.btnUnselect)
+        Me.Controls.Add(Me.radKeepTotal)
+        Me.Controls.Add(Me.radKeepInput)
+        Me.Controls.Add(Me.btnQuarter)
+        Me.Controls.Add(Me.btnTriple)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnRandomNum)
+        Me.Controls.Add(Me.btnHalf)
+        Me.Controls.Add(Me.btnDouble)
+        Me.Controls.Add(Me.txtIntegerInput)
+        Me.Controls.Add(Me.LblEnterInt)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Integer"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents LblEnterInt As Label
+    Friend WithEvents txtIntegerInput As TextBox
+    Friend WithEvents btnDouble As Button
+    Friend WithEvents btnHalf As Button
+    Friend WithEvents btnRandomNum As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btnTriple As Button
+    Friend WithEvents btnQuarter As Button
+    Friend WithEvents radKeepInput As RadioButton
+    Friend WithEvents radKeepTotal As RadioButton
+    Friend WithEvents btnUnselect As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,149 @@
+﻿Public Class Form1
+
+    'Create double variables that are private
+    Private dblIntegerInput, dblTotal As Double
+
+    'Converts the imput string into an interger, and clears the input
+    Private Sub Stringtoint()
+        'Converts the input into an integer
+        dblIntegerInput = Convert.ToDouble(txtIntegerInput.Text)
+
+        'If the radiobuttons aren't checked
+        If radKeepTotal.Checked = False And radKeepInput.Checked = False Then
+
+            'Clear the textbox
+            txtIntegerInput.Text = ""
+
+        End If
+    End Sub
+
+    'Keeps the total by converting the total to a string
+    Private Sub KeepTheTotal()
+        'If the total radio button is checked
+        If radKeepTotal.Checked = True Then
+
+            'Convert the double total to a string and put it in the textbox
+            txtIntegerInput.Text = Convert.ToString(dblTotal)
+
+        End If
+    End Sub
+
+    'When the double button is clicked, the number is checked then doubled.
+    Private Sub btnDouble_Click(sender As Object, e As EventArgs) Handles btnDouble.Click
+        'If the box has nothing in it, then display a message
+        If txtIntegerInput.Text = "" Then
+            'Display a message
+            MsgBox("Please provide a number in the textbox", vbExclamation)
+        Else
+            'Goes to the subfuntion to convert the input into an integer
+            Stringtoint()
+
+            'Times the input by 2
+            dblTotal = dblIntegerInput * 2
+
+            'Displays it's result
+            MsgBox("The double of " & dblIntegerInput & " is " & dblTotal)
+
+            'Checks if the box is check to keep the total in the inputbox
+            KeepTheTotal()
+        End If
+    End Sub
+
+    'When the half button is clicked, the input number is halved.
+    Private Sub btnHalf_Click(sender As Object, e As EventArgs) Handles btnHalf.Click
+        'If the input box has nothing in it, then display a message.
+        If txtIntegerInput.Text = "" Then
+            MsgBox("Please provide a number in the textbox", vbExclamation)
+        Else
+            'Goes to the subfuntion to convert the input into an integer
+            Stringtoint()
+
+            'Half the integer input
+            dblTotal = dblIntegerInput / 2
+
+            'Displays it's results
+            MsgBox("Half of " & dblIntegerInput & " is " & dblTotal)
+
+            'Checks if the box is check to keep the total in the inputbox
+            KeepTheTotal()
+        End If
+    End Sub
+
+
+
+    '  __                      __
+    ' |  |                    |  |
+    '_|  |_                  _|  |_
+    '\    /                  \    /
+    ' \  /                    \  /
+    '  \/ EXTRA FEATURES BELOW \/
+
+
+
+    'When the exit button is pressed, the application closes.
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        'Closes the main window.
+        Me.Close()
+    End Sub
+
+    'When the random number button is clicked, the application puts a random number in the input box.
+    Private Sub btnRandomNum_Click(sender As Object, e As EventArgs) Handles btnRandomNum.Click
+        'Declares the RandomNumber as an Integer
+        Dim RandomNumber As Integer
+
+        'Chooses a random number between 0 - 1000
+        RandomNumber = Math.Ceiling(Rnd() * 1000)
+
+        'Assigns the textbox as a random number
+        txtIntegerInput.Text = RandomNumber
+    End Sub
+
+    'If the Triple button is clicked, the input is multiplied by 4
+    Private Sub btnTriple_Click(sender As Object, e As EventArgs) Handles btnTriple.Click
+
+        'If the inputbox has nothing in it.
+        If txtIntegerInput.Text = "" Then
+            'Display a message
+            MsgBox("Please provide a number in the textbox", vbExclamation)
+        Else
+            'Goes to the subfuntion to convert the input into an integer
+            Stringtoint()
+
+            'Times the input by 3
+            dblTotal = dblIntegerInput * 3
+
+            'Displays it's result
+            MsgBox("The triple of " & dblIntegerInput & " is " & dblTotal)
+
+            'Checks if the box is check to keep the total in the inputbox
+            KeepTheTotal()
+        End If
+    End Sub
+
+    'If the Quarter button is clicked, the input is divided by 4
+    Private Sub btnQuarter_Click(sender As Object, e As EventArgs) Handles btnQuarter.Click
+        If txtIntegerInput.Text = "" Then
+            'Display a message
+            MsgBox("Please provide a number in the textbox", vbExclamation)
+        Else
+            'Goes to the subfuntion to convert the input into an integer
+            Stringtoint()
+
+            'Divides the input by 4
+            dblTotal = dblIntegerInput / 4
+
+            'Displays it's result
+            MsgBox("The quarter of " & dblIntegerInput & " is " & dblTotal)
+
+            'Checks if the box is check to keep the total in the inputbox
+            KeepTheTotal()
+        End If
+    End Sub
+
+    'When the Unselect button is clicked, it clears the radio buttons
+    Private Sub btnUnselect_Click(sender As Object, e As EventArgs) Handles btnUnselect.Click
+        'Uncircles the radio buttons
+        radKeepInput.Checked = False
+        radKeepTotal.Checked = False
+    End Sub
+End Class
